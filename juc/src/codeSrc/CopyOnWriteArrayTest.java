302b54d5d3ff8c0c13c2fc589b571e7a1b412ea7
zmh <623299342@qq.com>
2018-10-12 16:41:01 +0800

    copyOnWriteArray detail

--- juc/src/codeSrc/CopyOnWriteArrayTest.java
+++ juc/src/codeSrc/CopyOnWriteArrayTest.java
@@ -1,6 +1,7 @@
 package codeSrc;
 
 import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -14,31 +15,53 @@ import java.util.concurrent.CopyOnWriteArrayList;
  */
 public class CopyOnWriteArrayTest {
 
-    private static CopyOnWriteArrayList<Integer> pojos  = new CopyOnWriteArrayList<>();
-    private static List<Integer>                 pojos2 = new ArrayList<>();
+    private CopyOnWriteArrayList<String> list  = new CopyOnWriteArrayList<>();
+    private List<String>                 list2 = new ArrayList<>();
 
-    public static void main(String[] args) {
-        CopyOnWriteArrayTest test1 = new CopyOnWriteArrayTest();
-        new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
-                System.out.println(Thread.currentThread().getName() + " 执行第: " + i + "次");
-                test1.listAdd(1);
 
-            }
-        }).start();
-        new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
-                System.out.println(Thread.currentThread().getName() + " 执行第: " + i + "次");
-                test1.listAdd(2);
-            }
-        }).start();
+    /**
+     * copyOnWriteArray对数组进行操作后会刷新数组，避免ConcurrentModificationException
+     *
+     * @author zhaomenghui
+     * @createDate 2018/10/12
+     */
+    public static void main(String[] args) throws InterruptedException {
+        CopyOnWriteArrayTest test       = new CopyOnWriteArrayTest();
+        List<String>         initList   = test.list2;
+        int                  circleTime = 100;//循环次数
+        String               setVal0    = "first";//设置变量的值
+        String               setVal1    = "second";//设置变量的值
+        Thread               thread0    = test.getThread(initList, circleTime, setVal0);
+        Thread               thread1    = test.getThread(initList, circleTime, setVal1);
+        thread0.start();
+        thread1.start();
 
+        //检查元素数量是否正确
+        Thread.sleep(1000 * 2);//等待两个线程执行完成
+        int num1 = 0;
+        while (initList.remove(setVal0))
+            num1++;
+
+        System.out.println(setVal0 + "数量：" + num1 + "|" + setVal1 + "数量：" + initList.size());//
     }
 
-    private void listAdd(Integer val) {
-        pojos.add(val);
-        System.out.println(pojos);
-//        pojos2.add(val);
-//        System.out.println(pojos2);
+    private void listAdd(List list, String val) {
+        list.add(val);
+        System.out.println("list：" + list);//不在这个方法访问资源就不会报异常
+    }
+
+    private Thread getThread(List list, int circleTime, String setVal) {
+        return new Thread(() -> {
+            for (int i = 1; i <= circleTime; i++) {
+                try {
+                    String threadName = Thread.currentThread().getName();
+                    listAdd(list, setVal);
+                    System.out.println(threadName + " 执行第: " + i + "次");
+                } catch (ConcurrentModificationException e) {
+                    e.printStackTrace();
+                    return;//捕捉到异常就停止运行
+                }
+            }
+        });
     }
 }
