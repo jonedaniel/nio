71e40ee59ccdad1d9dcd5d4be0b061e56ce4a654
zmh <623299342@qq.com>
2018-10-20 11:08:06 +0800

    new start

---
+++ event_driven/src/basic/SelectorTest.java
@@ -0,0 +1,15 @@
+package basic;
+
+/**
+ * Title: SelectorTest
+ * Description: selector测试
+ *
+ * @author zhaomenghui
+ * @createDate 2018/10/19
+ * @version 1.0
+ */
+public class SelectorTest {
+    public static void main(String[] args) {
+        System.out.println("hello,event driven!");
+    }
+}
