d1fe3685b3cffba0cbcab6d73cb880f3f0c108e6
zmh_new <zhaomenghui93@163.com>
2018-11-24 19:31:56 +0800

    dead lock

---
+++ juc/src/concurency/DeadlockDemo.java
@@ -0,0 +1,39 @@
+package concurency;
+
+/**
+ * 死锁演示
+ *
+ * @createDate 2018/11/24
+ */
+public class DeadlockDemo {
+    private static String A = "A";
+    private static String B = "B";
+
+    public static void main(String[] args) {
+        new DeadlockDemo().deadLock();
+    }
+
+    private void deadLock() {
+        Thread thread1 = new Thread(() -> {
+            synchronized (A) {
+                try {
+                    Thread.currentThread().sleep(2000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (B) {
+                    System.out.println("1");
+                }
+            }
+        });
+        Thread thread2 = new Thread(() -> {
+            synchronized (B) {
+                synchronized (A) {
+                    System.out.println("2");
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+    }
+}
