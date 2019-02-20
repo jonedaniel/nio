2e528cf55a3ec5e70ae75a3849339811cc06499d
zmh2 <zmh.infomation@gmail.com>
2019-02-20 18:08:30 +0800

    rename nio server

---
+++ event_driven/nio-server/src/main/java/com/zmh/nioserver/http/HttpMessageReaderFactory.java
@@ -0,0 +1,18 @@
+package com.zmh.nioserver.http;
+
+import com.zmh.nioserver.IMessageReader;
+import com.zmh.nioserver.IMessageReaderFactory;
+
+/**
+ * Created by jjenkov on 18-10-2015.
+ */
+public class HttpMessageReaderFactory implements IMessageReaderFactory {
+
+    public HttpMessageReaderFactory() {
+    }
+
+    @Override
+    public IMessageReader createMessageReader() {
+        return new HttpMessageReader();
+    }
+}
